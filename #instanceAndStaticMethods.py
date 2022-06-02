249b655c07a35be91301b587eb9f49f6cc312bd1
jmanstino <justinmonstot@gmail.com>
2022-06-02 16:45:05 -0700

    finished change to cls from book

--- #instanceAndStaticMethods.py
+++ #instanceAndStaticMethods.py
@@ -21,14 +21,21 @@ class Book:
         self.weight = weight
 
     def __repr__(self):
-        return f"<Book {self.name}, {self.book_type}, weighing {self.weight} grams"
+        return f"<Book {self.name}, {self.book_type}, weighing {self.weight} grams>"
 
     @classmethod
     def hardcover(cls, name, page_weight):
-        return Book(name, Book.TYPES[0], page_weight + 100)
+        return cls(name, cls.TYPES[0], page_weight + 100)
+
+    ## NOTE: "book" and "cls" are both the class. They are interchangeable.
+    @classmethod
+    def paperback(cls, name, page_weight):
+        return cls(name, cls.TYPES[1], page_weight)
 
 ## How do I make sure I can only pass in hardcover or paperback instead of any string?
 ## by creating a "factory" in the class above.
-book = Book("Harry Potter", "hardcover", 1500)
+book = Book.hardcover("Harry Glover",  1500)
+softBook = Book.paperback("Jimmy John", 1400)
 
 print(book)
+print(softBook)
