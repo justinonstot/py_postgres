0a26bebb112b92f42a37b2b0d9a7522c5d3ba822
jmokoddi <justin.onstot@koddi.com>
2022-06-02 10:49:17 -0700

    changes to book class

--- #instanceAndStaticMethods.py
+++ #instanceAndStaticMethods.py
@@ -20,4 +20,15 @@ class Book:
         self.book_type = book_type
         self.weight = weight
 
-print(Book.TYPES)
+    def __repr__(self):
+        return f"<Book {self.name}, {self.book_type}, weighing {self.weight} grams"
+
+    @classmethod
+    def hardcover(cls, name, page_weight):
+        return Book(name, Book.TYPES[0], page_weight + 100)
+
+## How do I make sure I can only pass in hardcover or paperback instead of any string?
+## by creating a "factory" in the class above.
+book = Book("Harry Potter", "hardcover", 1500)
+
+print(book)
