f2815045e38a374f465a10bf499678760e591678
jmokoddi <justin.onstot@koddi.com>
2022-06-04 11:57:09 -0700

    class composition example

--- #ClassComposition.py
+++ #ClassComposition.py
@@ -1,15 +1,29 @@
 #ClassComposition
 
 ## Composition is used ALOT
+## Class that contains many classses...
+## For instance a Bookshelf has many Books.
 
 class BookShelf:
-    def __init__(self, quantity):
-        self.quantity = quantity
+    def __init__(self, *books):
+        self.books = books 
     
     def __str__(self):
-        return f"Bookshelf with {self.quantity} books."
+        return f"Bookshelf with {len(self.books)} books."
 
+class Book:
+    def __init__(self, name):
+       self.name = name
 
-shelf = BookShelf(300)
+    def __str__(self):
+        return f"Book {self.name}" 
+
+book = Book("Harry Potter")
+book2 = Book("Pride and Prejudice")
+shelf = BookShelf(book, book2)
 
 print(shelf)
+
+## It makes no sense to make a Book class inherit from Bookshelf... a Book is not a bookshelf.
+## instead, a bookshelf "HAS" many books.
+## instead put a constructor in above class to allow it to take in the count of books.
