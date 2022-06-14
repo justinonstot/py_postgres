b30e119d9ccf6c1f372dd94c2b5d068802df9f14
jmanstino <justinmonstot@gmail.com>
2022-06-14 11:58:24 -0700

    prog journal app 1

---
+++ PROGDIARY/app.py
@@ -0,0 +1,21 @@
+menu = """Please select one of the following options:
+1) Add new entry for today.
+2) View entries.
+3) Exit.
+
+Your selection: """
+
+
+welcome = "Welcome to the programming diary!"
+
+print(welcome)
+
+user_input = input(menu)
+while user_input != "3":
+    if user_input == "1":
+        print("Add new entry...")
+    elif user_input == "2":
+        print("View entries...")
+    else:
+        print("\n Invalid option, please try again!")
+    user_input = input(menu)
