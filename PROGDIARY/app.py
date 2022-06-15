74496a0a567b9593a0f35e75e4ae45f38eff9ec0
jmanstino <justinmonstot@gmail.com>
2022-06-15 08:49:16 -0700

    using walrus style

--- PROGDIARY/app.py
+++ PROGDIARY/app.py
@@ -1,3 +1,5 @@
+from database import add_entry, view_entries
+
 menu = """Please select one of the following options:
 1) Add new entry for today.
 2) View entries.
@@ -13,12 +15,14 @@ welcome = "Welcome to the programming diary!"
 
 print(welcome)
 
-user_input = input(menu)
-while user_input != "3":
+# user_input = input(menu) #Old style, non "walrus" assignment expression 
+while (user_input := user_input(menu)) != "3":
     if user_input == "1":
         print("Add new entry...")
+        add_entry()
     elif user_input == "2":
         print("View entries...")
+        view_entries()
     else:
         print("\n Invalid option, please try again!")
     user_input = input(menu)
