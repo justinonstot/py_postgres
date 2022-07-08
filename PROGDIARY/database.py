4afa053e3e7427d9e2ed1df6ba5c987a2baf8a5a
jmokoddi <justin.onstot@koddi.com>
2022-07-08 10:32:01 -0700

    sadfsadf

--- PROGDIARY/database.py
+++ PROGDIARY/database.py
@@ -1,15 +1,18 @@
-entries = [
-    {"content": "Today I started learning python", "date": "2019-01-01"},
-    {"content": "I create my first database", "date": "2019-02-01"},
-    {"content": "I finished writing my programming diary app", "date": "2019-03-01"},
-    {"content": "I finished writing my prog diary today!", "date": "2019-04-01"},
-    {"content": "Today I'm continuing my learning on programming", "date": "2019-04-02"},
-] 
+# entries = [
+#     {"content": "Today I started learning python", "date": "2019-01-01"},
+#     {"content": "I create my first database", "date": "2019-02-01"},
+#     {"content": "I finished writing my programming diary app", "date": "2019-03-01"},
+#     {"content": "I finished writing my prog diary today!", "date": "2019-04-01"},
+#     {"content": "Today I'm continuing my learning on programming", "date": "2019-04-02"},
+# ] 
+import sqlite3
+connection = sqlite3.connect("data.db")
 
+def create_table():
+    with connection:  # uses context manager to handle commit/rollback of the transaction.
+        connection.execute('CREATE TABLE entries (content TEXT, date TEXT);')
 
 def add_entry(entry_content, entry_date):
-    # entry_content = input("What have you learned today? ")
-    # entry_date = input("Enter the date (YYYY-MM-DD: ")
     entries.append({"content": entry_content, "date": entry_date})
 
 def get_entries():
