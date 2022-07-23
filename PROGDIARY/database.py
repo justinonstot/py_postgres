9ae260940a73f0bc9309e257c6e944f884e07501
Justin Onstot <justinmonstot@gmail.com>
2022-07-23 10:50:02 -0700

    changes on my mac

--- PROGDIARY/database.py
+++ PROGDIARY/database.py
@@ -1,18 +1,18 @@
 
 import sqlite3
-connection = sqlite3.connect("PROGDIARY\data.db")
+connection = sqlite3.connect("./data.db")
 
-entries = [] 
+# entries = [] 
 
 def create_table():
     with connection:  # uses context manager to handle commit/rollback of the transaction.
-        connection.execute('CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY, content TEXT, publication_date TEXT);')
+        connection.execute('CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);')
 
 def add_entry(entry_content, entry_date):
     # query = f"INSERT INTO entries VALUES (NULL,?,?');"
     # print(query)
     with connection:
-        connection.execute("INSERT INTO entries VALUES (NULL,?,?);", (entry_content, entry_date))
+        connection.execute("INSERT INTO entries VALUES (?,?);", (entry_content, entry_date))
 
 def get_entries():
-    return entries
+    entries = connection.execute("SELECT * from entries;")
